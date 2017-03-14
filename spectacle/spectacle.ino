a9aa3c9a2d76267f12bd43ab09a6ea0ab92ead2c
nadinecegepmatane <nadineducegep@gmail.com>
2017-03-13 23:19:57 -0400

    Debut factorisation des animations pas-a-pas

--- spectacle/spectacle.ino
+++ spectacle/spectacle.ino
@@ -35,6 +35,116 @@ Led couleurRougeFramboise = {.vert = 39, .rouge = 242, .bleu = 144};
 DiscoLed discoled(LED_NOMBRE, PIN);
 //Spectacle * spectacle;
 
+
+
+int listeModes[4];
+int positionDansProgramme;
+int positionDansAnimation = 0;
+int numeroAnimation = 0;
+
+int duree;
+
+int listeDurees[4];
+void setup()
+{
+listeModes[0] = MODE_ROTATION_ALTERNEE;
+listeModes[1] = MODE_CLIGNOTE_ROUGE;
+listeModes[2] = MODE_PROMENADE_ROUGE;
+listeModes[3] = MODE_TROIS_TOURS;
+listeDurees[0] = LONGUEUR_PROGRAMME_ROTATION_ALTERNEE;
+listeDurees[1] = LONGUEUR_PROGRAMME_CLIGNOTE_ROUGE;
+listeDurees[2] = LONGUEUR_PROGRAMME_PROMENAGE_ROUGE;
+listeDurees[3] = LONGUEUR_PROGRAMME_TROIS_TOURS;
+
+  mode = listeModes[numeroAnimation];
+  duree = listeDurees[numeroAnimation]; 
+
+  //spectacle = new Spectacle();
+  Serial.begin(9600);
+  bandeRougeFramboise = discoled.creerBandeCouleurUnie(couleurRougeFramboise);
+  bandeBlanche = discoled.creerBandeCouleurUnie(COULEUR_BLANC);
+  bandeBleuBlanc = creerBandeCouleurAlternee(couleurBleuQuebec, COULEUR_BLANC);
+  bandeBlancBleu = creerBandeCouleurAlternee(COULEUR_BLANC, couleurBleuQuebec);
+  bandeBlanchePixelRouge = discoled.creerBandeCouleurUnie(COULEUR_BLANC);
+  pixels = bandeRougeFramboise;
+}
+
+/*
+   Initialise la couleur des bandes
+   Paramètres : couleurs des led
+*/
+Led * creerBandeCouleurAlternee(Led couleur1, Led couleur2)
+{
+  Led * bandeCouleur = discoled.preparerBandeVide();
+  for (int position = 0; position < LED_NOMBRE; position += 2) // possible car le nombre est pair
+  {
+    bandeCouleur[position] = couleur1;
+    bandeCouleur[position + 1] = couleur2;
+
+  }
+  return bandeCouleur;
+}
+
+
+
+void loop()
+{
+  tour++;
+
+  positionDansProgramme = tour % LONGUEUR_PROGRAMME;
+
+  positionDansAnimation++;
+
+  if(positionDansAnimation == (duree+1)) 
+  {
+    positionDansAnimation = 0;  
+    numeroAnimation++;
+    if(numeroAnimation > 3) numeroAnimation = 0;
+  }
+  if(positionDansAnimation == 0)
+  {
+    mode = listeModes[numeroAnimation];
+    duree = listeDurees[numeroAnimation]; 
+  }
+  
+  if(mode == MODE_ROTATION_ALTERNEE)
+  {
+    pixels = animerRotationAlternee();
+  }
+  else if(mode == MODE_CLIGNOTE_ROUGE)
+  {
+    pixels = animerClignotementRouge();
+  }
+  else if(mode == MODE_PROMENADE_ROUGE)
+  {
+    pixels = animerPromenadeRouge();
+  }
+  else if(mode == MODE_TROIS_TOURS)
+  {
+    pixels = animerTroisTours();
+  }
+ 
+  discoled.afficher(pixels);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Led * animerRotationAlternee()
 {
     delay(300);
@@ -98,97 +208,6 @@ Led * animerTroisTours()
       pixels = bandeBlanchePixelRouge;
     }
    return pixels;
-}
-
-
-int listeModes[4];
-
-int listeDurees[4];
-void setup()
-{
-listeModes[0] = MODE_ROTATION_ALTERNEE;
-listeModes[1] = MODE_CLIGNOTE_ROUGE;
-listeModes[2] = MODE_PROMENADE_ROUGE;
-listeModes[3] = MODE_TROIS_TOURS;
-listeDurees[0] = LONGUEUR_PROGRAMME_ROTATION_ALTERNEE;
-listeDurees[1] = LONGUEUR_PROGRAMME_CLIGNOTE_ROUGE;
-listeDurees[2] = LONGUEUR_PROGRAMME_PROMENAGE_ROUGE;
-listeDurees[3] = LONGUEUR_PROGRAMME_TROIS_TOURS;
-
-  //spectacle = new Spectacle();
-  Serial.begin(9600);
-  bandeRougeFramboise = discoled.creerBandeCouleurUnie(couleurRougeFramboise);
-  bandeBlanche = discoled.creerBandeCouleurUnie(COULEUR_BLANC);
-  bandeBleuBlanc = creerBandeCouleurAlternee(couleurBleuQuebec, COULEUR_BLANC);
-  bandeBlancBleu = creerBandeCouleurAlternee(COULEUR_BLANC, couleurBleuQuebec);
-  bandeBlanchePixelRouge = discoled.creerBandeCouleurUnie(COULEUR_BLANC);
-  pixels = bandeRougeFramboise;
-}
-
-/*
-   Initialise la couleur des bandes
-   Paramètres : couleurs des led
-*/
-Led * creerBandeCouleurAlternee(Led couleur1, Led couleur2)
-{
-  Led * bandeCouleur = discoled.preparerBandeVide();
-  for (int position = 0; position < LED_NOMBRE; position += 2) // possible car le nombre est pair
-  {
-    bandeCouleur[position] = couleur1;
-    bandeCouleur[position + 1] = couleur2;
-
-  }
-  return bandeCouleur;
-}
-
-
-int positionDansProgramme;
-int positionDansAnimation = 0;
-int numeroAnimation = 0;
-
-int duree;
-
-void loop()
-{
-  tour++;
-
-  positionDansProgramme = tour % LONGUEUR_PROGRAMME;
-
-  positionDansAnimation++;
-  
-  mode = listeModes[numeroAnimation];
-  duree = listeDurees[numeroAnimation]; 
-
-  if(positionDansAnimation == duree) 
-  {
-    positionDansAnimation = 0;  
-    numeroAnimation++;
-    if(numeroAnimation > 3) numeroAnimation = 0;
-  }
-  
-  //if (positionDansProgramme < LONGUEUR_PROGRAMME_ROTATION_ALTERNEE) mode = MODE_ROTATION_ALTERNEE;  
-  //else if ((positionDansProgramme - LONGUEUR_PROGRAMME_ROTATION_ALTERNEE) < LONGUEUR_PROGRAMME_CLIGNOTE_ROUGE) mode = MODE_CLIGNOTE_ROUGE;
-  //else if ((positionDansProgramme - LONGUEUR_PROGRAMME_ROTATION_ALTERNEE - LONGUEUR_PROGRAMME_CLIGNOTE_ROUGE) < LONGUEUR_PROGRAMME_PROMENAGE_ROUGE) mode = MODE_PROMENADE_ROUGE;
-  //else if ((positionDansProgramme - LONGUEUR_PROGRAMME_ROTATION_ALTERNEE - LONGUEUR_PROGRAMME_CLIGNOTE_ROUGE - LONGUEUR_PROGRAMME_PROMENAGE_ROUGE) < LONGUEUR_PROGRAMME_TROIS_TOURS) mode = MODE_TROIS_TOURS;
-
-  if(mode == MODE_ROTATION_ALTERNEE)
-  {
-    pixels = animerRotationAlternee();
-  }
-  else if(mode == MODE_CLIGNOTE_ROUGE)
-  {
-    pixels = animerClignotementRouge();
-  }
-  else if(mode == MODE_PROMENADE_ROUGE)
-  {
-    pixels = animerPromenadeRouge();
-  }
-  else if(mode == MODE_TROIS_TOURS)
-  {
-    pixels = animerTroisTours();
-  }
- 
-  discoled.afficher(pixels);
 }
 
 
