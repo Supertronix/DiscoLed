8893b621f5f929f694e459bd6a12cbbdc13fb801
nadinecegepmatane <nadineducegep@gmail.com>
2017-03-20 20:37:19 -0400

    On peut rouler deux programmes en parallele

---
+++ tests/thread-scheduler/thread-scheduler.ino
@@ -0,0 +1,36 @@
+#include <Scheduler.h>
+
+void initialiserBonjourMonde()
+{
+  Serial.println("Initialiser Bonjour Monde");  
+}
+
+void bouclerBonjourMonde()
+{
+  Serial.println("Boucle Bonjour Monde");  
+  delay(10);
+}
+
+void initialiserPaysDesMerveilles()
+{
+  Serial.println("Initialiser Pays Des Merveilles");  
+}
+
+void bouclerPaysDesMerveilles()
+{
+  Serial.println("Boucle Pays Des Merveilles");  
+  delay(10);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initialiser");  
+  Scheduler.start(initialiserBonjourMonde, bouclerBonjourMonde);
+  Scheduler.start(initialiserPaysDesMerveilles, bouclerPaysDesMerveilles);  
+}
+
+void loop() {
+  delay(10);
+  Serial.println("boucle principale");  
+
+}
