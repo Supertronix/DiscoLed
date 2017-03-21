e64f33d74fb8fc9b55ff5c88cd0337010e71cf30
nadinecegepmatane <nadineducegep@gmail.com>
2017-03-21 15:07:14 -0400

    Programme qui drive rondelle et bande lumineuse en parallele

---
+++ spectacles/rondelle-et-bande/rondelle-et-bande.ino
@@ -0,0 +1,36 @@
+#include <Scheduler.h>
+
+void initialiserRondelle()
+{
+  Serial.println("Initialiser Rondelle");  
+}
+
+void bouclerRondelle()
+{
+  Serial.println("Boucle Rondelle");  
+  delay(10);
+}
+
+void initialiserBande()
+{
+  Serial.println("Initialiser Bande");  
+}
+
+void bouclerBande()
+{
+  Serial.println("Boucle Bande");  
+  delay(10);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initialiser");  
+  Scheduler.start(initialiserRondelle, bouclerRondelle);
+  Scheduler.start(initialiserBande, bouclerBande);  
+}
+
+void loop() {
+  delay(10);
+  Serial.println("boucle principale");  
+
+}
