bff50bcc3efce50a17f070fbfbd4eb0b48b67326
nadineducegep <nadineducegep@gmail.com>
2018-02-25 20:22:40 -0500

    Sauvegarde de la version WS2812 de DiscoLed

---
+++ librairies/DiscoLed/WS2812/DiscoLed.cpp
@@ -0,0 +1,90 @@
+#include "DiscoLed.h"
+
+DiscoLed::DiscoLed(int nombre, int pin)
+{	
+	masque = digitalPinToBitMask(pin);
+	port = portOutputRegister(digitalPinToPort(pin));
+	portMode = portModeRegister(digitalPinToPort(pin));
+	anneau = new WS2812(pin);
+	anneau->setOutput(pin);
+	this->pin = pin;
+	this->nombre = nombre;
+}
+
+Led * DiscoLed::preparerBandeVide()
+{
+  return (Led*)malloc(this->nombre*3*sizeof(uint8_t));
+}
+
+Led * DiscoLed::creerBandeCouleurUnie(Led couleur)
+{
+  Led * bandeCouleur = this->preparerBandeVide();
+  for(int position = 0; position < this->nombre; position++)
+  {
+    bandeCouleur[position] = couleur;
+  }
+  return bandeCouleur;
+}
+
+void DiscoLed::afficher(Led* pixels)
+{
+	//anneau->set_crgb_at(2, couleur);
+	*portMode |= masque;
+	anneau->ws2812_sendarray_mask((uint8_t*)pixels,3*this->nombre, masque,(uint8_t*) port,(uint8_t*) portMode); 
+	//anneau->sync();
+
+}
+
+/*
+ * Initialise la couleur du led : 
+ * Parametre : niveau de rouge, niveau de vert, niveau de bleu
+ */
+Led DiscoLed::creerCouleur(int rouge, int vert, int bleu)
+{
+    Led couleur;
+    couleur.rouge = rouge;
+    couleur.vert = vert;
+    couleur.bleu = bleu;
+    return couleur;
+}
+
+#define TAILLE_SPECTACLE 8
+
+Spectacle::Spectacle()
+{
+	this->capaciteAnimations = TAILLE_SPECTACLE;
+	this->listeAnimations = new Animation *[TAILLE_SPECTACLE]; 
+	this->nombreAnimations = 0;
+	this->animationCourante = 0;
+}
+Spectacle::Spectacle(DiscoLed * discoled)
+{
+	this->discoled = discoled;
+	this->capaciteAnimations = TAILLE_SPECTACLE;
+	this->listeAnimations = new Animation *[TAILLE_SPECTACLE]; 
+	this->nombreAnimations = 0;
+	this->animationCourante = 0;
+}
+void Spectacle::ajouterAnimation(Animation * animation)
+{
+	// TODO : gerer la croissance au-dela de TAILLE_SPECTACLE plus tard
+	this->listeAnimations[nombreAnimations++] = animation;
+}
+void Spectacle::jouerAnimation()
+{
+	if(nombreAnimations < 1) return;
+	
+	this->listeAnimations[animationCourante]->animer();		
+	this->sauterAnimation();
+}
+bool Spectacle::jouerAnimationSansSauter()
+{
+	if(nombreAnimations < 1) return;
+	return this->listeAnimations[animationCourante]->animer();		
+}
+
+void Spectacle::sauterAnimation()
+{
+	animationCourante++;
+	if(animationCourante > nombreAnimations) animationCourante = 0;
+}
---
+++ librairies/DiscoLed/WS2812/DiscoLed.h
@@ -0,0 +1,63 @@
+#include <WS2812.h>
+#ifndef DISCOLED_H
+#define DISCOLED_H
+
+struct Led
+{ 
+  uint8_t vert;
+  uint8_t rouge;
+  uint8_t bleu;
+};
+
+const Led COULEUR_BLANC = {255, 255, 255};
+const Led COULEUR_ROUGE = {0, 255, 0};
+const Led COULEUR_BLEU = {0, 0, 255};
+const Led COULEUR_VERT = {255, 0, 0};
+
+class DiscoLed
+{
+	
+	protected:
+		uint8_t masque;
+		const volatile uint8_t * port;
+		volatile uint8_t * portMode;
+		WS2812 * anneau;
+		int nombre;
+		int pin;
+
+
+	public:
+		DiscoLed(int nombre, int pin);
+		Led * preparerBandeVide();
+		Led * creerBandeCouleurUnie(Led couleur);
+		void afficher(Led* pixels);
+		Led creerCouleur(int rouge, int vert, int bleu);
+};
+
+class Animation
+{
+	public:
+		int duree;
+		bool (*animer)();
+};
+
+class Spectacle
+{
+  protected:
+    Animation ** listeAnimations;
+	DiscoLed * discoled;
+    int nombreAnimations;
+    int capaciteAnimations;
+    int duree;
+	int animationCourante;
+
+  public:
+    Spectacle();
+    Spectacle(DiscoLed *);
+    void ajouterAnimation(Animation *);
+	void jouerAnimation();
+	bool jouerAnimationSansSauter();
+	void sauterAnimation();
+};
+
+#endif
