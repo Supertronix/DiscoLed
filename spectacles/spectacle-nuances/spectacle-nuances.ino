a0caaf9b6f4d1ab8829b2f8d2ebe3283fd293962
nadineducegep <nadineducegep@gmail.com>
2018-02-28 13:25:44 -0500

    Repartition des animations en fichiers transportables

--- spectacles/spectacle-nuances/spectacle-nuances.ino
+++ spectacles/spectacle-nuances/spectacle-nuances.ino
@@ -8,44 +8,24 @@ DiscoLed discoled(NOMBRE, PIN, HORLOGE);
 Spectacle spectacle(&discoled); // peut-etre plutot memoire dynamique
 
 uint8_t moment;
-bool animerArcEnCiel()
-{
-  Serial.print("animerArcEnCiel() ");
-  moment = millis() >> 4;
-  for(int position = 0; position < NOMBRE; position++)
-  {
-    uint8_t p = moment-position*8;
-    pixels[position] = FabriqueLed::convertirHSV( (uint32_t)p * 359 / 256, 255, 255);
-  }
-  discoled.afficher(pixels);
-}
 
 Led couleurBleuQuebec = {.rouge = 0, .vert = 92, .bleu = 22};
 Led * bandeBleuBlanc;
 Led * bandeBlancBleu;
-bool animerAlternance()
-{
-  moment = millis() >> 4;
-  if(moment%40 > 20)
-    pixels = bandeBlancBleu;
-  else 
-    pixels = bandeBleuBlanc;
-  discoled.afficher(pixels);
-  //if(positionDansAnimation%2 == 0) 
-}
+
 
 void setup() 
 {
   Serial.begin(9600);
   pixels = new Led[NOMBRE];
 
-  bandeBleuBlanc = discoled.creerBandeCouleurAlternee(couleurBleuQuebec, COULEUR_BLANC); 
-  bandeBlancBleu = discoled.creerBandeCouleurAlternee(COULEUR_BLANC, couleurBleuQuebec); 
+  initialiserAlternance();
 
   Animation * animationArcEnCiel = new Animation();
   animationArcEnCiel->animer = animerArcEnCiel;
   animationArcEnCiel->duree = 300;
   spectacle.ajouterAnimation(animationArcEnCiel);
+  
   Animation * animationAlternance = new Animation();
   animationAlternance->animer = animerAlternance;
   animationAlternance->duree = 400;
