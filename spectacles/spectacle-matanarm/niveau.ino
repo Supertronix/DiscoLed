9788c719fed2ea67d953f313fac7ab89e9865c55
nadineducegep <nadineducegep@gmail.com>
2018-03-01 13:27:45 -0500

    Code pour niveau du chariot

---
+++ spectacles/spectacle-matanarm/niveau.ino
@@ -0,0 +1,31 @@
+void preparerNiveau(DiscoLed* discoled)
+{
+  discoled->blanchir();
+}
+Led couleurAlliance = COULEUR_ROUGE;
+int niveau = 5;
+bool animerNiveau(DiscoLed* discoled)
+{  
+  niveau = convertirChariotNiveau(chariot);
+  //Serial.print("Niveau "); Serial.println(niveau);
+
+  for(int position = 0; position < niveau; position++) // on pourrait optimiser et juste dessiner la derniere sauf que si le niveau monte trop vite
+  {
+    discoled->pixels[position] = couleurAlliance;
+  }
+  discoled->afficher(discoled->pixels);
+}
+
+void libererNiveau(DiscoLed* discoled)
+{
+  
+}
+
+#define FACTEUR_NIVEAU 0.5
+
+int convertirChariotNiveau(int chariot)
+{
+  niveau = (int)(FACTEUR_NIVEAU*chariot);
+  if(niveau >= NOMBRE) niveau = NOMBRE - 1;
+  return niveau;
+}
--- spectacles/spectacle-matanarm/spectacle-matanarm.ino
+++ spectacles/spectacle-matanarm/spectacle-matanarm.ino
@@ -6,16 +6,24 @@
 #define NOMBRE 67
 
 DiscoLed discoled(NOMBRE, PIN, HORLOGE);
-Spectacle spectacle(&discoled); // peut-etre plutot memoire dynamique
-uint8_t moment;
+Spectacle spectacle(&discoled); 
+Spectacle spectacleNiveau(&discoled); 
+uint8_t moment = 0;
 
 void setup() 
 {
   Serial.begin(9600);
   Wire.begin(4);
   Wire.onReceive(recevoirCommandeRio);
-  
 
+  Animation * animationNiveau = new Animation();
+  animationNiveau->preparer = preparerNiveau;
+  animationNiveau->animer = animerNiveau;
+  animationNiveau->liberer = libererNiveau;
+  animationNiveau->duree = 400;
+  spectacleNiveau.ajouterAnimation(animationNiveau);
+
+/*
   Animation * animationVagues = new Animation();
   animationVagues->preparer = preparerVagues;
   animationVagues->animer = animerVagues;
@@ -50,12 +58,32 @@ void setup()
   animationRayures->liberer = libererRayures;
   animationRayures->duree = 400;
   spectacle.ajouterAnimation(animationRayures);
+  */
  }
+
+#define MODE_NORMAL 0
+#define MODE_NIVEAU 1
+#define MODE_FLASH 2
+
+int mode = MODE_NIVEAU;
+int chariot = 0;
 
 void loop() 
 {
-  //Serial.println("bouclerBandeDroite()");
-  spectacle.jouerAnimation();
+  //Serial.println("loop()");
+  moment++; 
+  //if(moment%2 == 0) 
+  chariot++;  // simulation de reception
+  //Serial.print("chariot "); Serial.println(chariot);
+  
+  switch(mode)
+  {
+    case MODE_NORMAL : spectacle.jouerAnimation(); break;
+    case MODE_NIVEAU : spectacleNiveau.jouerAnimation(); break;
+    //case MODE_FLASH : spectacleFlash.jouerAnimation(); break;
+    default: spectacle.jouerAnimation(); break;
+  }
+  
   delay(1);
 }
 
@@ -68,6 +96,5 @@ void recevoirCommandeRio(int nombre)
     Serial.println(lettre);
   }
 }
-
 
 
