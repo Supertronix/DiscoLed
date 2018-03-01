c9efc99df46377aa19db31dc2a8c0046dcaf4521
nadineducegep <nadineducegep@gmail.com>
2018-03-01 11:50:35 -0500

    Les deux bandes sont soudées ensembles - retrait du scheduler

--- spectacles/spectacle-matanarm/spectacle-matanarm.ino
+++ spectacles/spectacle-matanarm/spectacle-matanarm.ino
@@ -1,37 +1,49 @@
 #include <DiscoLed.h>
-#include <Scheduler.h>
-
-#define DROIT 0
-#define GAUCHE 1
 
 #define PIN_DROITE 3
 #define HORLOGE_DROITE 4
-#define PIN_GAUCHE 2
-#define HORLOGE_GAUCHE 5
 #define NOMBRE 67
 
 DiscoLed discoledDroit(NOMBRE, PIN_DROITE, HORLOGE_DROITE);
-DiscoLed discoledGauche(NOMBRE, PIN_GAUCHE, HORLOGE_GAUCHE);
 Spectacle spectacleDroit(&discoledDroit); // peut-etre plutot memoire dynamique
-Spectacle spectacleGauche(&discoledGauche); // peut-etre plutot memoire dynamique
 
 uint8_t moment;
-DiscoLed& discoled = discoledGauche;
+DiscoLed& discoled = discoledDroit;
 
 void setup() 
 {
+  Serial.begin(9600);
   //Serial.begin(9600);
-  //Serial.println("initialisation principale");  
+  //Serial.println("initialiserBandeDroite()");
 
-  Scheduler.start(initialiserBandeDroite, bouclerBandeDroite);  
-  Scheduler.start(initialiserBandeGauche, bouclerBandeGauche);  
+  Animation * animationVagues = new Animation();
+  animationVagues->preparer = preparerVagues;
+  animationVagues->animer = animerVagues;
+  animationVagues->liberer = libererVagues;
+  animationVagues->duree = 400;
+  spectacleDroit.ajouterAnimation(animationVagues);
+  
+  Animation * animationArcEnCiel = new Animation();
+  animationArcEnCiel->preparer = preparerArcEnCiel;
+  animationArcEnCiel->animer = animerArcEnCiel;
+  animationArcEnCiel->liberer = libererArcEnCiel;
+  animationArcEnCiel->duree = 400;
+  spectacleDroit.ajouterAnimation(animationArcEnCiel);
+/*
+  initialiserCollision(&discoledDroit);
+  Animation * animationCollision = new Animation();
+  animationCollision->animer = animerCollision;
+  animationCollision->duree = 400;
+  spectacleDroit.ajouterAnimation(animationCollision);
+  */
 }
 
 
 void loop() 
 {
-  delay(10);
-  //Serial.println("boucle principale");  
+  //Serial.println("bouclerBandeDroite()");
+  spectacleDroit.jouerAnimation();
+  delay(1);
 }
 
 
